549cc71488f3abdec1966e9f8a1bfd5555c44f8b
Stein Rune Risa <steinrr@gmail.com>
2024-01-21 14:00:24 +0100

    Initial commit

---
+++ convert_lifetlist_ao.py
@@ -0,0 +1,41 @@
+import csv
+import sys
+import subprocess
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askopenfilename()
+
+aooutput = ''
+i = 0
+
+with open(file_path, newline='') as csvfile:
+    lifelistreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in lifelistreader:
+        if i == 0:
+            aooutput = 'Artsnavn	Lokalitetsnavn	Fra dato	Fra klokkeslett	Antall	Kjønn'
+        else:
+            # Artsnavn
+            aooutput += row[0]
+            # Lokalitetsnavn
+            aooutput += '\t' + row[4]
+            # Fra dato
+            aooutput += '\t' + row[2][8:10] + '.' + row[2][5:7] + '.' + row[2][0:4]
+            # Fra klokkeslett
+            aooutput += '\t' + row[3][0:5]
+            # Antall
+            aooutput += '\t' + row[7]
+            # Kjønn
+            aooutput += '\t' + row[8]
+            
+        aooutput += '\n'
+        i +=1
+        
+
+subprocess.run("pbcopy", text=True, input=aooutput)
+messagebox.showinfo("showinfo", 'Converted ' + str(i) + ' records.\nResult copied to clipboard.')
+
+
